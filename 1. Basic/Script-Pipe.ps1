cb695c6c61a99aa2925dcf4d5fc24b2c10f401f4
Daniel Sirait <37093+DNS@users.noreply.github.com>
2026-04-26 15:15:03 +0700

    pipe & args support

--- 1. Basic/Script-Pipe.ps1
+++ 1. Basic/Script-Pipe.ps1
@@ -1,14 +1,16 @@
 <#
 
+Script-Pipe.ps1 — Accept values passed from pipelines or arguments
+
 Example:
-'John', 'Adam',  | Script-Pipe.ps1
+'John', 'Adam' | Script-Pipe.ps1
 Script-Pipe.ps1 'Obito', 'Madara'
 #>
 
 [CmdletBinding()]
 Param(
-	[Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromRemainingArguments=$true)] $InputObject
-	#[Parameter(Mandatory=$false)] [Switch] $Help
+	[Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromRemainingArguments=$true)] $InputObject,
+	[Parameter(Mandatory=$false)] [Switch] $Help
 )
 
 
