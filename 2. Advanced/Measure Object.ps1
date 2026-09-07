bbf109e99ce6e76b3f70ee4247efc93d19f83f7a
Daniel Sirait <37093+DNS@users.noreply.github.com>
2026-09-07 14:49:03 +0700

    Add Measure Object script for statistical calculations

---
+++ 2. Advanced/Measure Object.ps1
@@ -0,0 +1,14 @@
+@(5,7,4,8,2,9) | Measure-Object -Average -Sum -Minimum -Maximum -StandardDeviation
+
+
+<#
+
+Count             : 6
+Average           : 5.83333333333333
+Sum               : 35
+Maximum           : 9
+Minimum           : 2
+StandardDeviation : 2.63944438597722
+Property          :
+
+#>
